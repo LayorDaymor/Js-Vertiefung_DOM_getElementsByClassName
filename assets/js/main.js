eea49656b16386173f3fa3d30448379d8ab62fac
LayorDaymor <ahmad.karbouj98@gmail.com>
2020-06-06 13:00:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    const exampels =document.querySelectorAll(".example");
+    exampels.forEach(element => {
+        element.style.background="black";
+        element.style.color="white"
+        
+    });
+    
+}
